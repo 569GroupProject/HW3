be372d01d0ad06d30cb3c50b94ff522cd8a17474
JunJie Xie <jx170@georgetown.edu>
2023-03-29 23:13:43 -0400

    Update MoneyManager.sol
    add withdrawMoney and Address

--- MoneyManager.sol
+++ MoneyManager.sol
@@ -5,6 +5,8 @@ contract MoneyManager {
     uint public receivedAmount;
     uint public timeOut;
 
+    event Withdrawal(address indexed recipient, uint amount);
+
     function receiveMoney() public payable {
         receivedAmount += msg.value;
         timeOut = block.timestamp + 1 minutes;
@@ -13,4 +15,21 @@ contract MoneyManager {
     function currentBalance() public view returns (uint) {
         return address(this).balance;
     }
+
+    function withdrawMoney() public {
+        require(block.timestamp >= timeOut, "Withdrawal not allowed yet");
+        address payable recipient = payable(msg.sender);
+        uint balance = currentBalance();
+        recipient.transfer(balance);
+        emit Withdrawal(recipient, balance);
+    }
+
+    function withdrawToAddress(address payable _address) public {
+        require(block.timestamp >= timeOut, "Cannot withdraw before timeout");
+        require(address(this).balance > 0, "No balance to withdraw");
+        uint balance = currentBalance();
+        _address.transfer(balance);
+        emit Withdrawal(_address, balance);
+    }
 }
+
